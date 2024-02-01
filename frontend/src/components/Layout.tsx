3733463c6b9b3a085a12d06ac8ef0303603a2776
gwenael9 <gwenaelgueho@gmail.com>
2024-02-01 16:06:15 +0100

    resetDb done + hot reload front

---
+++ frontend/src/components/Layout.tsx
@@ -0,0 +1,35 @@
+import Head from "next/head";
+import Link from "next/link";
+import { ReactNode } from "react";
+
+interface LayoutProps {
+  children: ReactNode;
+  title: string;
+}
+
+export default function Layout({ children, title }: LayoutProps) {
+  return (
+    <>
+      <Head>
+        <title>{title}</title>
+        <meta name="description" content="petites annonces" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="favicon.ico" />
+      </Head>
+      <header>
+        <ul className="flex justify-center gap-4">
+          <li>
+            <Link href="/">Home</Link>
+          </li>
+          <li>
+            <Link href="/read">Read</Link>
+          </li>
+          <li>
+            <Link href="/create">Create</Link>
+          </li>
+        </ul>
+      </header>
+      <main className="main-content">{children}</main>
+    </>
+  );
+}
