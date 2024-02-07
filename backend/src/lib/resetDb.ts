c1967f78a5ffe0b810ec396701cc2c55a7a349c0
Léo Brival <leo.brival@gmail.com>
2024-02-07 18:08:27 +0100

    Register & Login

--- backend/src/lib/resetDb.ts
+++ backend/src/lib/resetDb.ts
@@ -2,29 +2,29 @@ import Test from "../entities/test.entity";
 import db from "./datasource";
 
 async function clearDB() {
-    const runner = db.createQueryRunner();
+  const runner = db.createQueryRunner();
 
-    // supprime toutes les tables
-    await Promise.all(
-      db.entityMetadatas.map(async (entity) =>
-        runner.query(`DROP TABLE IF EXISTS ${entity.tableName} CASCADE`)
-      )
-    );
+  // supprime toutes les tables
+  await Promise.all(
+    db.entityMetadatas.map(async (entity) =>
+      runner.query(`DROP TABLE IF EXISTS "${entity.tableName}" CASCADE`)
+    )
+  );
 
-    await db.synchronize();
+  await db.synchronize();
 }
 
 async function main() {
-    await db.initialize();
-    await clearDB();
+  await db.initialize();
+  await clearDB();
 
-    // creation de resetTest dans la table Test
-    const resetTest = Test.create({
-        text: "cleaaaar"
-    });
+  // creation de resetTest dans la table Test
+  const resetTest = Test.create({
+    text: "cleaaaar",
+  });
 
-    // sauvegarde
-    await resetTest.save();
+  // sauvegarde
+  await resetTest.save();
 }
 
 main();
