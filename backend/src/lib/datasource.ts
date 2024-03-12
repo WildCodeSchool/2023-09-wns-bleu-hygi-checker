dccb47c2d3f39f7420d4a11ef63589e2a3fbf07d
LucieChev <lchevanche@hotmail.fr>
2024-03-12 11:24:31 +0100

    tests e2e fix error push

--- backend/src/lib/datasource.ts
+++ backend/src/lib/datasource.ts
@@ -4,9 +4,9 @@ import User from "../entities/user.entity";
 
 dotenv.config();
 
-const db =  new DataSource({
+const db = new DataSource({
   type: "postgres",
-  host: process.env.DB_HOST || "db" ||"testDB",
+  host: process.env.DB_HOST || "db" || "testDB",
   port: parseInt(process.env.DB_PORT || "0") || 5432,
   username: process.env.DB_USER || "postgres",
   password: process.env.DB_PASS || "postgres",
