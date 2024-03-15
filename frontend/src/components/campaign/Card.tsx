e60b6eb6014a0d18bffe292bb5cbfabd9adaccde
LucieChev <lchevanche@hotmail.fr>
2024-03-15 11:46:13 +0100

    fix error

--- frontend/src/components/campaign/Card.tsx
+++ frontend/src/components/campaign/Card.tsx
@@ -1,7 +1,9 @@
+import Image from "next/image";
 import { Badge } from "../ui/badge";
 import { Button } from "../ui/button";
 import { Card, CardTitle, CardContent, CardFooter } from "../ui/card";
 import { Switch } from "../ui/switch";
+import { useRouter } from "next/router";
 
 export interface DataCardProps {
   id: number;
@@ -10,16 +12,29 @@ export interface DataCardProps {
 }
 
 export default function CampaignCard({ data }: { data: DataCardProps }) {
+  const router = useRouter();
   return (
-    <Card className="flex flex-col bg-card text-white w-[350px]">
-      <img src="/card.jpg" alt="image" className="w-full rounded-t-lg" />
+    <Card className="flex flex-col w-[350px]">
+      <Image
+        src="https://source.unsplash.com/random?wallpapers"
+        alt="image"
+        className="w-full rounded-t-lg"
+        width={250}
+        height={0}
+      />
       <CardContent className="flex justify-between p-6 items-center">
         <CardTitle className="text-2xl">Campaign #{data.id}</CardTitle>
         <Switch />
       </CardContent>
       <CardFooter className="flex justify-between">
-        <Badge variant={"white"}>{data.url} URLs</Badge>
-        <Button>Voir</Button>
+        <Badge>{data.url} URLs</Badge>
+        <Button
+          onClick={() => {
+            router.push("/dashboard/campaign/details");
+          }}
+        >
+          Voir
+        </Button>
       </CardFooter>
     </Card>
   );
