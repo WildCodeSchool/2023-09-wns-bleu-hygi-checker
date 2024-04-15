4823973033c2997b904c17821809df803de6079e
LucieChev <lchevanche@hotmail.fr>
2024-04-15 15:51:28 +0200

    fix error e2e

--- e2e-tests/src/login.spec.ts
+++ e2e-tests/src/login.spec.ts
@@ -1,6 +1,6 @@
-import { connect, disconnect} from './helpers';
+import { connect, disconnect } from "./helpers";
 import { clearDB } from "../../backend/src/lib/datasource";
-import { test, expect } from '@playwright/test';
+import { test, expect } from "@playwright/test";
 import UserService from "../../backend/src/services/user.service";
 
 test.beforeAll(connect);
@@ -12,17 +12,18 @@ const password = "test";
 
 async function createUser() {
   const userService = new UserService();
-  await userService.createUser({email, password});
+  await userService.createUser({ email, password });
 }
 
-test('can connect with correct credentials', async ({ page }) => {
+test("can connect with correct credentials", async ({ page }) => {
   await createUser();
 
-  await page.goto('http://localhost:3000/auth/login');
-  await page.getByTestId('login-email').fill(email);
-  await page.getByTestId('login-password').fill(password);
-  await page.getByRole('button', {name: "Connexion"}).click();
-  await expect(page.getByTestId('watch-button')).toBeVisible();
+  await page.goto("http://localhost:3000/auth/login");
+  await page.getByTestId("login-email").fill(email);
+  await page.getByTestId("login-password").fill(password);
+  await page.getByRole("button", { name: "Connexion" }).click();
+  await expect(
+    page.getByRole("button", { name: "Connexion" })
+  ).toBeVisible();
   // await page.pause();
 });
-
