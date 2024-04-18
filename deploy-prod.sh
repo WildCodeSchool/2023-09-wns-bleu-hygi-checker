89bfb74796a057369e23a5778a005d5bf69c8ea6
Marc-Antoine B <115265331+HazeFury@users.noreply.github.com>
2024-04-18 18:56:53 +0200

    up

--- deploy-prod.sh
+++ deploy-prod.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 git fetch origin && git reset --hard origin/main && git clean -f -d && \
 docker compose -f docker-compose.prod.yml down && \
-docker container prune --force && \
-docker image prune -a --force && \
 docker compose -f docker-compose.prod.yml pull && \
 docker compose -f docker-compose.prod.yml --env-file .env.prod up -d;
