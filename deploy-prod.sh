22a3486e8d4d4c13f837b6805e3e935a9af08f9c
Marc-Antoine B <115265331+HazeFury@users.noreply.github.com>
2024-04-19 12:33:23 +0200

    test 6

--- deploy-prod.sh
+++ deploy-prod.sh
@@ -2,4 +2,4 @@
 git fetch origin && git reset --hard origin/main && git clean -f -d && \
 docker compose -f docker-compose.prod.yml down && \
 docker compose -f docker-compose.prod.yml pull && \
-docker compose -f docker-compose.prod.yml --env-file .env.prod up -d --remove-orphans;
+docker compose -f docker-compose.prod.yml --env-file .env.prod up -d;
