d1605135f441c9336a25e0463d5f4f305d73a86a
Léo Brival <leo.brival@gmail.com>
2024-04-19 18:12:56 +0200

    Add node-cron lib and check-url script

---
+++ backend/src/utils/cronTasks.ts
@@ -0,0 +1,14 @@
+import cron from "node-cron";
+// import { listActiveCampaigns } from "../path_to_checkActiveCampaigns_function";
+
+export function startCronJobs() {
+  // Cron job to check active campaigns every hour
+  cron.schedule("0 * * * *", () => {
+    console.info(
+      "Exécution de la vérification des campagnes actives chaque heure."
+    );
+    // listActiveCampaigns();
+  });
+}
+
+// startCronJobs() -> index.ts
