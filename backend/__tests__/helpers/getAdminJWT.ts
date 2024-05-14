cb0af3bd332b0793986f397a985749228060318e
gwenael9 <gwenaelgueho@gmail.com>
2024-05-14 16:32:27 +0200

    secret_key -> jwt_private_key

--- backend/__tests__/helpers/getAdminJWT.ts
+++ backend/__tests__/helpers/getAdminJWT.ts
@@ -7,12 +7,11 @@ export default async function () {
     email: "admin@app.com",
     password: "adminadmin",
   }).save();
-  // const JWT = await jwt.sign({ userId: admin.id }, process.env.SECRET_KEY);
 
   const JWT = await new SignJWT({ userId: admin.id })
     .setProtectedHeader({ alg: "HS256", typ: "jwt" })
     .setExpirationTime("1m")
-    .sign(new TextEncoder().encode(`${process.env.SECRET_KEY}`));
+    .sign(new TextEncoder().encode(`${process.env.JWT_PRIVATE_KEY}`));
 
   return { admin, JWT };
 }
