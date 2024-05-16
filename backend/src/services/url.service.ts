598daa966bf978628fd0effbb09df5504c98b7b4
Léo Brival <leo.brival@gmail.com>
2024-05-16 10:34:46 +0200

    Update Resolver for Apollo Studio

--- backend/src/services/url.service.ts
+++ backend/src/services/url.service.ts
@@ -207,7 +207,7 @@ export default class UrlService {
       const endTime = Date.now();
 
       const responseTime = endTime - startTime;
-      const responseDate = response.headers.get("date") || "Non disponible";
+      const responseDate = response.headers.get("date") || "Not available";
 
       return {
         status: response.status,
