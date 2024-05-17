c4c9e2c38ad5df2b7fd7fc0087dfcdf8fa2bd202
Marc-Antoine B <115265331+HazeFury@users.noreply.github.com>
2024-05-17 02:29:04 +0200

    WIP about createCampaign. Started by added the campaign query in campaignList and corrected the campaignCard test

--- frontend/src/__tests__/campaignCard.test.tsx
+++ frontend/src/__tests__/campaignCard.test.tsx
@@ -1,4 +1,4 @@
-import CampaignCard from "@/components/dashboard/CampaignCard";
+import CampaignCard from "@/components/campaign/CampaignCard";
 import "@testing-library/jest-dom";
 import { render, screen } from "@testing-library/react";
 
@@ -6,15 +6,25 @@ describe("CampaignCard component", () => {
   it("renders correctly", () => {
     const view = render(
       <CampaignCard
-        campaign={{
-          title: "teeeest",
-          url: "urllll",
-          urlNumber: 4,
+        data={{
+          name: "campaign test",
+          image:
+            "https://images.unsplash.com/photo-1419064642531-e575728395f2?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxfDB8MXxyYW5kb218MHx8d2FsbHBhcGVycyxsYW5kc2NhcGV8fHx8fHwxNzE1OTA0NTE0&ixlib=rb-4.0.3&q=80&utm_campaign=api-credit&utm_medium=referral&utm_source=unsplash_source&w=1080",
+          id: 2,
+          isWorking: true,
+          isMailAlert: true,
+          intervalTest: 60,
+          userId: "d5d31072-f28a-4ea7-b971-3f457cfdfa5d",
+          urls: {
+            urlPath: "https://www.github.com/",
+            type: "PAGE",
+            id: 1,
+          },
         }}
       />
     );
-    expect(screen.getByText(/teeeest/)).toBeInTheDocument();
-    expect(screen.getByText(/4/)).toBeInTheDocument();
+    expect(screen.getByText(/campaign test/)).toBeInTheDocument();
+    expect(screen.getByText(/Voir/)).toBeInTheDocument();
     expect(view.baseElement).toMatchSnapshot();
   });
 });
